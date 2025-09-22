db98eb5089bfdd9ffec827ec0a034a22cc50c0a7
farisalshehria-create <farisalshehri.a@gmail.com>
2025-09-22 12:56:23 +0300

    add class customer

---
+++ bank/customer.py
@@ -0,0 +1,11 @@
+class Customer:
+    #راح اضمنه بعدين في سي اس في
+    def __init__(self, account_id,first_name,last_name , password ,checking_balance = 0.0, savings_balance= 0.0):
+        # نستقبل قيم العميل و اضفت رصيد
+        self.account_id= account_id
+        self.first_name= first_name 
+        self.last_name= last_name 
+        self.password= password 
+        self.checing_balance= float(checking_balance)
+        self.savings_balance= float(savings_balance)
+        # القيم بدون الطلبات
