263d44266b7afb0fd2682766ac3755cb9727fc0b
farisalshehria-create <farisalshehri.a@gmail.com>
2025-09-24 08:43:53 +0300

    class account update

--- bank/account.py
+++ bank/account.py
@@ -7,9 +7,15 @@ class Account:
                             #صحح mony ب money
         def deposit(self, mony):
             self.balance +=mony
+            return self.balance
+            if mony <=0:
+                raise ValueError("worong ! invalid number")
+            
         def withdraw(self,mony):
             #تاكد من المبلغ المسحوب
+            #لذل الرصيد لايسمح
             if mony <= self.balance:
+                raise ValueError("you balance is not enough")
                 #بعد التاكيد ان المبلغ كافي وحول يخصم المبلغ
                 self.balance -= mony 
                 return True
