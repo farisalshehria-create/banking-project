8eb220797151648061ec8c9b122e43c30dc2e669
farisalshehria-create <farisalshehri.a@gmail.com>
2025-09-27 03:06:43 +0300

    reject -101> and move the file :)

--- tests/test_overdraft.py
+++ tests/test_overdraft.py
@@ -7,5 +7,14 @@ class TestOverdraft(unittest.TestCase):
         acc .withdraw(165.0) # 100-156= -65
 
         self.assertEqual(acc.balance, -100.0)# -65 -35=-100
+
+        
+    #رفض السحب اذا الرصيد -100
+    def test_reject_min100(self):
+        acc =Account(100.0)
+        with self.assertRaises(ValueError):
+            acc .withdraw(166.0) #100 - 166 = -66 - 35 = -101
+            
 if __name__ == "___main_":
     unittest.main()
+
